c69713e45117bacfc7aa8debb4347a989a3383fb
Xuanru <danxuanru@gmail.com>
2022-08-02 02:04:21 -0700

    HW01

---
+++ ut_main.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "helloworld.h"
+
+TEST(HELLOWORLD, PrintHelloWorld)
+{
+    string output = PrintHelloWorld();
+    string compare = "Hello World! \n";
+    ASSERT_EQ(output, compare);
+}
+
+int main(int argc, char **argv)
+{
+    testing ::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
